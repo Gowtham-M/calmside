3196d5e301894bb0277856e326bff1eee576abe6
Jyothirmayi Chinda <jyothi.c@siraconsultinginc.com>
2024-09-11 15:09:26 +0530

    added dashboard page details and package lock added to ignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AdminPage from './pages/AdminPage/index';
-import UserOrderPage from './pages/UserOrderPage/index';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AdminPage from "./pages/AdminPage/index";
+import UserOrderPage from "./pages/UserOrderPage/index";
+import DashboardPage from "./pages/DashboardPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<DashboardPage />} />
         <Route path="/:company" element={<UserOrderPage />} />
         <Route path="/admin/:company" element={<AdminPage />} />
       </Routes>
