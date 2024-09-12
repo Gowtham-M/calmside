1d4fb3e1b7f434f85519092564125519baf8c6e3
Jyothirmayi Chinda <jyothi.c@siraconsultinginc.com>
2024-09-12 18:05:20 +0530

    ignored eslint warning

--- frontend/src/pages/SuperUserPage/addAdmin.js
+++ frontend/src/pages/SuperUserPage/addAdmin.js
@@ -14,6 +14,7 @@ const AdminManagement = () => {
 
   useEffect(() => {
     fetchAdmins();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchAdmins = async () => {
